abf5df96ea1fb1dae0a8d7d357eefe91ab293dd8
GivanFH <ivan_2333@outlook.com>
2024-12-17 17:00:15 -0600

    origin undefined

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,16 +2,11 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
+        const whitelist = [process.env.HIDALGO_URL,process.env.HIDALGO_URL_FRONT, process.env.LOCALHOST]
+
         console.log("Origin:", origin);
-        if (process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
-
-        //Esto tambien arregla el error de CORS por ser undefined
-        // if (!origin || whitelist.includes(origin))
-
-        if (whitelist.includes(origin)) {
+        
+        if (!origin || whitelist.includes(origin)) {
             callback(null, true)
         } else {
             callback(new Error('Error de CORS'))
